8c5cfcfe5b221996a1d80dca9b837fcc6c99092b
mahin33ferdous <mahinferdousshoily@gmail.com>
2023-10-07 02:38:03 -1100

    created payment payment component based on booking id

--- index.js
+++ index.js
@@ -157,7 +157,13 @@ async function run(){
       res.send(result)
     })
 
+    app.get('/bookings/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id: new ObjectId(id)}
+      const result= await bookingCollection.findOne(query)
+      res.send(result)
 
+    })
 
     //creating user collection on the database
     app.post('/users',async(req,res)=>{
