aff908fe0d4d4d8a1ceb17ace8630c6d6fd2f9d3
mahin33ferdous <mahinferdousshoily@gmail.com>
2023-10-07 17:20:32 -1100

    installing stripe and creating paymentIntent api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors=require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 var jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port=process.env.PORT || 5000
 
 const app=express()
@@ -172,6 +173,30 @@ async function run(){
       res.send(result)
 
     })
+
+    //payment stripe
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price=booking.price;
+      const amount=price * 100;
+    
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd",
+        amount: amount,
+        // click to Paymentintent to get more documentation
+        "payment_method_types": [
+          "card"
+        ]
+      });
+    
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+    
+    
 
   }
   finally{
